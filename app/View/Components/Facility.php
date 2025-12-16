2d92b6ffcea3ca5dcc605cc7ba7d6544d9362506
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-16 16:00:50 +0700

    add facility component and template

---
+++ app/View/Components/Facility.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class Facility extends Component
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.facility');
+    }
+}
