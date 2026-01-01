cb56d4f7ddeb9b477354eec2f220721e05de075f
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-01-02 04:09:27 +0700

    add Facility model with UUID support and fillable attributes

---
+++ app/Models/Facility.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Facility extends Model
+{
+    use HasUuids;
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $fillable = ['name', 'description', 'image'];
+}
