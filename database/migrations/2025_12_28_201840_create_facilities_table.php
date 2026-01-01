45cfcb4d79e56583e7b2da99bef9d07e7d3b79ef
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-01-02 04:08:31 +0700

    add migration for facilities table with fields for id, name, description, and image

---
+++ database/migrations/2025_12_28_201840_create_facilities_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('facilities');
+    }
+};
