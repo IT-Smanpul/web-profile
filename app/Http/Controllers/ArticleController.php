6f2147e6784fd9cae9c15603a30f77c0937612ee
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-10 12:37:31 +0700

    enhance ArticleController with CRUD functionality and dynamic titles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,25 +2,113 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Article;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\Article\CreateArticleRequest;
+use App\Http\Requests\Article\UpdateArticleRequest;
 
 class ArticleController extends Controller
 {
-    public function index(): View
+    private string $appName;
+
+    public function __construct()
     {
-        return view('dashboard.berita.index');
+        $this->appName = Config::get('app.name');
     }
 
-    public function create() {}
+    public function index(): View
+    {
+        return view('dashboard.berita.index', [
+            'title' => "Berita - $this->appName",
+            'articles' => Article::latest()->paginate(6),
+        ]);
+    }
 
-    public function store(Request $request) {}
+    public function create(): View
+    {
+        return view('dashboard.berita.create', [
+            'title' => "Tambah Berita - $this->appName",
+        ]);
+    }
 
-    public function show($id) {}
+    public function store(CreateArticleRequest $request): RedirectResponse
+    {
+        $path = $request->file('thumbnail')->store('images/articles');
 
-    public function edit($id) {}
+        Article::create([
+            ...$request->validated(),
+            'author_id' => Auth::id(),
+            'thumbnail' => $path,
+        ]);
 
-    public function update(Request $request, $id) {}
+        return to_route('berita.index');
+    }
 
-    public function destroy($id) {}
+    public function show(Article $article): View
+    {
+        return view('detail', [
+            'title' => "$article->title - $this->appName",
+            'article' => $article,
+            'others' => Article::where('slug', '!=', $article->slug)->latest()->take(3)->get(),
+        ]);
+    }
+
+    public function edit(Article $article): View
+    {
+        return view('dashboard.berita.edit', [
+            'title' => "Edit Berita - $this->appName",
+            'article' => $article,
+        ]);
+    }
+
+    public function update(UpdateArticleRequest $request, Article $article): RedirectResponse
+    {
+        if ($request->hasFile('thumbnail')) {
+            Storage::delete($article->thumbnail);
+            $path = $request->file('thumbnail')->store('images/articles');
+
+            $article->update([
+                ...$request->validated(),
+                'thumbnail' => $path,
+            ]);
+        } else {
+            $article->update($request->validated());
+        }
+
+        return to_route('berita.index');
+    }
+
+    public function destroy(Article $article): RedirectResponse
+    {
+        Storage::delete($article->thumbnail);
+        $article->delete();
+
+        return to_route('berita.index');
+    }
+
+    public function publish(Article $article): RedirectResponse
+    {
+        $article->update(['published' => true]);
+
+        return to_route('berita.index');
+    }
+
+    public function unpublish(Article $article): RedirectResponse
+    {
+        $article->update(['published' => false]);
+
+        return to_route('berita.index');
+    }
+
+    public function preview(Article $article): View
+    {
+        return view('dashboard.berita.preview', [
+            'title' => "Preview Berita - $this->appName",
+            'article' => $article,
+        ]);
+    }
 }
