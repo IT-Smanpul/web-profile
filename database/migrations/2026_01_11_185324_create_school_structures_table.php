ffa72768daeb187ef122764b5ab5bf882b1d7be0
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-11 19:05:12 +0700

    feat: create migration for school_structures table to store school staff data

---
+++ database/migrations/2026_01_11_185324_create_school_structures_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('school_structures', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('nip');
+            $table->string('position')->unique();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('school_structures');
+    }
+};
