68569b0bffecb5adea7ebdf938be55c7bcbd7007
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-14 12:15:45 +0700

    feat: create migration for employees table with necessary fields

---
+++ database/migrations/2026_01_14_075657_create_employees_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('nip')->unique()->nullable();
+            $table->string('role')->default('guru');
+            $table->string('position');
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
