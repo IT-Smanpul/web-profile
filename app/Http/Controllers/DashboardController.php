27e00bc1faf33cc6985cb3283cd6fcae9b972f7e
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-14 15:57:10 +0700

    feat: create DashboardController for displaying dashboard metrics

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Models\Employee;
+use App\Models\Achievement;
+use Illuminate\Contracts\View\View;
+
+class DashboardController extends Controller
+{
+    public function __invoke(): View
+    {
+        return view('dashboard.index', [
+            'title' => 'Dashboard - SMA Negeri 10 Pontianak',
+            'totalArticles' => Article::all()->count(),
+            'totalAchievements' => Achievement::all()->count(),
+            'totalGurus' => Employee::guru()->get()->count(),
+            'totalStaffs' => Employee::staff()->get()->count(),
+            'latestArticles' => Article::latest()->get(),
+            'latestAchievements' => Achievement::latest()->get(),
+        ]);
+    }
+}
