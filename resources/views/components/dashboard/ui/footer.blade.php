c10448e25c18af8091525e838ea84ebaf59c178e
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-19 07:35:32 +0700

    feat: simplify footer layout by removing unnecessary max-width constraint

--- resources/views/components/dashboard/ui/footer.blade.php
+++ resources/views/components/dashboard/ui/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="mx-auto w-full max-w-7xl px-6 py-3.5 text-sm">
+<footer class="mx-auto w-full px-6 py-3.5 text-sm">
   <div class="flex items-center justify-between gap-3 max-lg:flex-col">
     <p class="text-base-content text-center">
       &copy; {{ now()->year }} <a class="text-primary" href="https://github.com/IT-Smanpul" target="_blank">TIM IT
