8aeb6586bf9fcd85fd8e2cca7aa5a0076e041a92
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-22 09:28:00 +0700

    feat: update LogoutController to specify return type as RedirectResponse

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Session;
 
 class LogoutController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): RedirectResponse
     {
         Auth::logout();
         Session::invalidate();
