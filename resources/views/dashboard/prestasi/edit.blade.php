91db6ffbdbf70054a33f7d3c7aacef82870fe5b7
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-26 09:31:45 +0700

    feat: remove title sections from create, edit, and index views for a cleaner layout

--- resources/views/dashboard/prestasi/edit.blade.php
+++ resources/views/dashboard/prestasi/edit.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Edit Prestasi')
-
 @section('main')
   <livewire:dashboard::prestasi.edit :achievement="$achievement" />
 @endsection
--- resources/views/dashboard/prestasi/index.blade.php
+++ resources/views/dashboard/prestasi/index.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Prestasi')
-
 @section('main')
   <livewire:dashboard::prestasi.index />
 @endsection
