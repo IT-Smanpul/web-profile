0dc78883dc8b4b025244756c9ee20fb651155376
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-26 09:30:04 +0700

    feat: remove title sections and unused markup from fasilitas views for a cleaner layout

--- resources/views/dashboard/fasilitas/create.blade.php
+++ resources/views/dashboard/fasilitas/create.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Tambah Fasilitas')
-
 @section('main')
   <livewire:dashboard::fasilitas.create />
 @endsection
--- resources/views/dashboard/fasilitas/edit.blade.php
+++ resources/views/dashboard/fasilitas/edit.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Tambah Fasilitas')
-
 @section('main')
   <livewire:dashboard::fasilitas.edit :facility="$facility" />
 @endsection
