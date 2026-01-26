8816ac94f7dca244e5df7344347a86fcee884794
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-26 09:30:28 +0700

    feat: remove title sections from guru-staff views for a cleaner layout

--- resources/views/dashboard/guru-staff/edit.blade.php
+++ resources/views/dashboard/guru-staff/edit.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Edit Guru & Staff')
-
 @section('main')
   <livewire:dashboard::guru-staff.edit :employee="$employee" />
 @endsection
--- resources/views/dashboard/guru-staff/index.blade.php
+++ resources/views/dashboard/guru-staff/index.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Guru dan Staff')
-
 @section('main')
   <livewire:dashboard::guru-staff.index />
 @endsection
