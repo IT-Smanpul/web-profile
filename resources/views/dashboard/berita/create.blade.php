405375611c00f8920df480d8127b9371daa2f1bb
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-26 09:29:00 +0700

    feat: remove title sections from berita views for cleaner layout

--- resources/views/dashboard/berita/create.blade.php
+++ resources/views/dashboard/berita/create.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Tambah Berita')
-
 @section('main')
   <livewire:dashboard::berita.create />
 @endsection
--- resources/views/dashboard/berita/index.blade.php
+++ resources/views/dashboard/berita/index.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Berita')
-
 @section('main')
   <livewire:dashboard::berita.index />
 @endsection
