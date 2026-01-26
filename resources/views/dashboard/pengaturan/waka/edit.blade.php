d07a3ca8147687944cb4e2f86fbf0f54266f8635
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-26 09:31:22 +0700

    feat: remove title sections from create, edit, and index views for a cleaner layout

--- resources/views/dashboard/pengaturan/waka/edit.blade.php
+++ resources/views/dashboard/pengaturan/waka/edit.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Edit Wakil Kepala Sekolah')
-
 @section('main')
   <livewire:dashboard::pengaturan.waka.edit :waka="$waka" />
 @endsection
--- resources/views/dashboard/pengaturan/waka/index.blade.php
+++ resources/views/dashboard/pengaturan/waka/index.blade.php
@@ -1,7 +1,5 @@
 @extends('components.dashboard.layout')
 
-@section('title', 'Kepala Sekolah')
-
 @section('main')
   <livewire:dashboard::pengaturan.waka.index />
 @endsection
