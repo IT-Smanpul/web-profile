33653574cb4c6d2eacc225c09d70c7b00f0d20d8
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-27 09:20:33 +0700

    fix: rename Suggestions class to Suggestion for consistency

--- app/Models/Suggestion.php
+++ app/Models/Suggestion.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
-class Suggestions extends Model
+class Suggestion extends Model
 {
     use HasUuids;
 
