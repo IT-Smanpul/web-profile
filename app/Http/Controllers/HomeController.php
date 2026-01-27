fdf37b9a91faea5a6a305d88cb3bcabcee341d68
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-27 11:14:09 +0700

    feat: add HomeController with methods for handling facilities, ekskul, achievements, and articles views

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ekskul;
+use App\Models\Article;
+use App\Models\Facility;
+use App\Models\Achievement;
+use Illuminate\Contracts\View\View;
+
+class HomeController extends Controller
+{
+    public function fasilitas(): View
+    {
+        return view('fasilitas', [
+            'title' => "Fasilitas - $this->appName",
+            'facilities' => Facility::paginate(6),
+        ]);
+    }
+
+    public function ekskul(): View
+    {
+        return view('ekskul', [
+            'title' => "Ekstrakurikuler - $this->appName",
+            'ekskuls' => Ekskul::paginate(6),
+        ]);
+    }
+
+    public function prestasi(): View
+    {
+        return view('prestasi', [
+            'title' => "Prestasi - $this->appName",
+            'achievements' => Achievement::paginate(6),
+        ]);
+    }
+
+    public function berita(): View
+    {
+        return view('berita.index', [
+            'title' => "Berita - $this->appName",
+            'articles' => Article::paginate(6),
+        ]);
+    }
+}
