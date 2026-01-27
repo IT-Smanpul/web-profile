1aa019cbc0bb212218dcfd853d3212b97b2c5e6d
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-27 11:14:27 +0700

    feat: update DatabaseSeeder to include AchievementSeeder, FacilitySeeder, and EkskulSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        $this->call([UserSeeder::class, ArticleSeeder::class]);
+        $this->call([UserSeeder::class, ArticleSeeder::class, AchievementSeeder::class, FacilitySeeder::class, EkskulSeeder::class]);
     }
 }
