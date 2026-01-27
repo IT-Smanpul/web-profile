0f4c554cd8786b2e12e13ede3b1b217c72c8f678
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-27 10:03:08 +0700

    fix: rename image column to photo and make it nullable in facilities table

--- database/migrations/2025_12_28_201840_create_facilities_table.php
+++ database/migrations/2025_12_28_201840_create_facilities_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('name');
             $table->string('description');
-            $table->string('image');
+            $table->string('photo')->nullable();
             $table->timestamps();
         });
     }
