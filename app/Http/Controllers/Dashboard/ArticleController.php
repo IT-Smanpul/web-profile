54ac91eb59c9ee2ed4467ed2cfdef96193a6daea
rikarani <46773591+rikarani@users.noreply.github.com>
2026-01-30 09:48:19 +0700

    chore: update article titles to use setTitle method for consistency

--- app/Http/Controllers/Dashboard/ArticleController.php
+++ app/Http/Controllers/Dashboard/ArticleController.php
@@ -11,21 +11,21 @@ class ArticleController extends Controller
     public function index(): View
     {
         return view('dashboard.berita.index', [
-            'title' => "Berita - $this->appName",
+            'title' => $this->setTitle('List Berita'),
         ]);
     }
 
     public function create(): View
     {
         return view('dashboard.berita.create', [
-            'title' => "Tambah Berita - $this->appName",
+            'title' => $this->setTitle('Tambah Berita'),
         ]);
     }
 
     public function show(Article $article): View
     {
         return view('berita.detail', [
-            'title' => "$article->title - $this->appName",
+            'title' => $this->setTitle($article->title),
             'article' => $article,
             'others' => Article::where('slug', '!=', $article->slug)->latest()->take(3)->get(),
         ]);
@@ -34,7 +34,7 @@ class ArticleController extends Controller
     public function edit(Article $article): View
     {
         return view('dashboard.berita.edit', [
-            'title' => "Edit Berita - $this->appName",
+            'title' => $this->setTitle('Edit Berita'),
             'article' => $article,
         ]);
     }
